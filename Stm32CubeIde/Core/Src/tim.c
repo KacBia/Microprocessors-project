306b0cc0289cd1270924f6131eca8c98e1fe2bbc
Shadi Frayhat <shadifarah332@gmail.com>
2025-01-21 22:17:00 +0100

    trials

--- Stm32CubeIde/Core/Src/tim.c
+++ Stm32CubeIde/Core/Src/tim.c
@@ -131,7 +131,7 @@ void MX_TIM9_Init(void)
   htim9.Instance = TIM9;
   htim9.Init.Prescaler = 95;
   htim9.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim9.Init.Period = 19999;
+  htim9.Init.Period = 20000-1;
   htim9.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim9.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim9) != HAL_OK)
